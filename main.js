f0c2deeeed7b684474fe4b9b5fa011babd42e8a5
Lucas <lucasjapa10@hotmail.com>
2022-05-25 16:08:29 -0300

    api working

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
 const cep = document.querySelector('#cepInput');
 const adicionarBtn = document.querySelector('#adicionar');
 adicionarBtn.addEventListener('click', (event) => {
-  event.preventDefault();
+    event.preventDefault();
     const API_LINK = `https://viacep.com.br/ws/${cep.value}/json/`
-    let resultado
     fetch(API_LINK) 
-    .then(response => response.json())
-    .then( response => console.log(response))
-    .then ( response => resultado = response)
-        
+    .then( response => response.json())
+    .then( response => {
+        criarElemento(response);
+    })
     .catch(error => console.log(error));
-    criarElemento(resultado);
+
 
     cep.value = "";
     cep.focus();
 
 })
 
-function criarElemento(valor) {
+function criarElemento(valor, tipo) {
     console.log(valor)
+
     let local = document.querySelector('#resultado');
     let div = document.createElement('div');
     div.setAttribute('class', 'containerResultado');
     local.appendChild(div);
 
-    let texto = document.createElement('h2');
+    let texto = document.createElement('p');
     texto.setAttribute('class', 'textoInfo');
-    texto.value = valor
+    texto.textContent = valor.cep
     div.appendChild(texto);
 }
 
